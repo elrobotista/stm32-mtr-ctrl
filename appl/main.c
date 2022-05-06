38adf277876af023c3831f60e6ac001401da08b3
Asimov <asimov@asimov.com>
2022-05-06 17:07:11 -0500

    ADC read POT channel working.
    Basic configuration for single conversion for POT channel working.

--- appl/main.c
+++ appl/main.c
@@ -3,22 +3,26 @@
 #include "libopencm3/stm32/rcc.h"
 #include "libopencm3/stm32/gpio.h"
 #include "libopencm3/stm32/timer.h"
+#include "libopencm3/stm32/adc.h"
 #include "libopencm3/cm3/systick.h"
 #include "libopencm3/cm3/nvic.h"
 
 #define SYSTICK_FRQ (1000)
-
 /* Lowest priority. As defined by the upper nibble. */
 /* LSB nibble is don't care. */
 #define SYSTICK_PRIO (0xF0)
 
+/******************************************************************************
+ * GPIO Configuration.
+******************************************************************************/
 #define GPIO_PORT_LED1 (GPIOA)
 #define GPIO_PIN_LED1 (GPIO5)
-#define RCC_PORT_LED1 (RCC_GPIOA)
 
+/******************************************************************************
+ * PWM Configuration.
+******************************************************************************/
 /* PWM definition / macros. */
 #define PWM_PORT_X (GPIOA)
-#define PWM_RCC_PORT (RCC_GPIOA)
 #define PWM_GPIO_U (GPIO8)
 #define PWM_GPIO_V (GPIO9)
 #define PWM_GPIO_W (GPIO10)
@@ -32,14 +36,48 @@
 #error("Invalid PWM config: ARR register out of bounds.")
 #endif
 
+/******************************************************************************
+ * ADC Configuration.
+******************************************************************************/
+#define ADC_CH_PHA_IFBK (1u)
+#define ADC_GPIO_PHA_IFBK (GPIO0)
+#define ADC_PORT_PHA_IFBK (GPIOA)
+
+#define ADC_CH_VBUS (2u)
+#define ADC_GPIO_VBUS (GPIO1)
+#define ADC_PORT_VBUS (GPIOA)
+
+#define ADC_CH_POT (1u)
+#define ADC_PORT_POT (GPIOB)
+#define ADC_GPIO_POT (GPIO1)
+
+#define ADC_CH_PHB_IFBK (7u)
+#define ADC_GPIO_PHB_IFBK (GPIO1)
+#define ADC_PORT_PHB_IFBK (GPIOC)
+
+#define ADC_CH_PHC_IFBK (6u)
+#define ADC_GPIO_PHC_IFBK (GPIO0)
+#define ADC_PORT_PHC_IFBK (GPIOC)
+
+#define ADC_CH_TEMP_SENS (8u)
+#define ADC_GPIO_TEMP_SENS (GPIO2)
+#define ADC_PORT_TEMP_SENS (GPIOC)
+
 static void hw_init(void);
 static void gpio_init(void);
 static void clock_init(void);
 static void pwm_init(void);
+static void adc_init(void);
+static void adc_setup(void);
 
 int main(void) {
+  volatile uint16_t tmp_adc = 0u;
   hw_init();
-  while(true);
+  while(true) {
+    adc_start_conversion_regular(ADC3);
+    while(!adc_eoc(ADC3));
+    tmp_adc = adc_read_regular(ADC3);
+  }
   return 0;
 }
 
@@ -58,11 +96,15 @@ static void hw_init(void) {
   clock_init();
   gpio_init();
   pwm_init();
+  adc_init();
+  /* adc_setup(); */
 }
 
 static void gpio_init(void) {
-  rcc_periph_clock_enable(RCC_PORT_LED1);
-  rcc_periph_clock_enable(PWM_RCC_PORT);
+  rcc_periph_clock_enable(RCC_GPIOA);
+  rcc_periph_clock_enable(RCC_GPIOB);
+  rcc_periph_clock_enable(RCC_GPIOC);
+
   /* LED output configuration. */
   gpio_mode_setup(
     GPIO_PORT_LED1,
@@ -97,6 +139,28 @@ static void gpio_init(void) {
   gpio_port_config_lock(
     PWM_PORT_X,
     (PWM_GPIO_U | PWM_GPIO_V | PWM_GPIO_W)
+  );
+
+  /* ADC input configuration. */
+  gpio_mode_setup(
+    GPIOA,
+    GPIO_MODE_ANALOG,
+    GPIO_PUPD_NONE,
+    (ADC_GPIO_PHA_IFBK | ADC_GPIO_VBUS)
+  );
+
+  gpio_mode_setup(
+    GPIOB,
+    GPIO_MODE_ANALOG,
+    GPIO_PUPD_NONE,
+    (ADC_GPIO_POT)
+  );
+
+  gpio_mode_setup(
+    GPIOC,
+    GPIO_MODE_ANALOG,
+    GPIO_PUPD_NONE,
+    (ADC_GPIO_PHB_IFBK | ADC_GPIO_PHC_IFBK | ADC_GPIO_TEMP_SENS)
   );
 }
 
@@ -186,12 +250,40 @@ static void pwm_init(void) {
   timer_set_oc_mode(TIM1, TIM_OC3, TIM_OCM_PWM1);
   timer_set_oc_value(TIM1, TIM_OC3, 0);
 
+  /* Timer used to generate ADC sync. pulse. */
+  timer_set_oc_slow_mode(TIM1, TIM_OC4);
+  timer_set_oc_mode(TIM1, TIM_OC4, TIM_OCM_PWM2);
+  timer_set_oc_value(TIM1, TIM_OC4, (PWM_TMR_PRD - 1));
+
   /* Master out configuration. */
   timer_enable_oc_output(TIM1, TIM_OC1);
   timer_enable_oc_output(TIM1, TIM_OC2);
   timer_enable_oc_output(TIM1, TIM_OC3);
+  timer_enable_oc_output(TIM1, TIM_OC4);
 
   /* Need to enable main output even if break / deadtime features are not used. */
   timer_enable_break_main_output(TIM1);
   timer_enable_counter(TIM1);
 }
+
+static void adc_init(void) {
+  uint8_t adc_conv_ch[] = {
+    ADC_CH_POT,
+  };
+
+  rcc_periph_clock_enable(RCC_ADC12);
+  rcc_periph_clock_enable(RCC_ADC34);
+
+  adc_power_off(ADC3);
+  adc_set_clk_prescale(ADC3, ADC_CCR_CKMODE_DIV2);
+  adc_set_single_conversion_mode(ADC3);
+  /* Equivalent to Software Start trigger. */
+  adc_disable_external_trigger_regular(ADC3);
+  adc_set_right_aligned(ADC3);
+  adc_set_sample_time_on_all_channels(ADC3, ADC_SMPR_SMP_19DOT5CYC);
+  adc_set_regular_sequence(ADC3, sizeof(adc_conv_ch), adc_conv_ch);
+  adc_set_resolution(ADC3, ADC_CFGR1_RES_12_BIT);
+  adc_calibrate(ADC3);
+  adc_power_on(ADC3);
+}
+
